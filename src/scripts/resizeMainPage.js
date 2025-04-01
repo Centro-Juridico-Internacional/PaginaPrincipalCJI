baada85f53d8a489a4746b2128d16132530232da
Desarrollo1 <desarrollo1@centrojuridicointernacional.com>
2025-04-01 15:54:52 -0500

    [BUILD] MainPage.astro

---
+++ src/scripts/resizeMainPage.js
@@ -0,0 +1,24 @@
+window.addEventListener('resize', () => {
+    const headerElement = document.getElementById('header');
+    const headerHeight = headerElement ? headerElement.offsetHeight : 0; // Si 'header' es null, usa 0 como altura.
+    const screenHeight = window.innerHeight;
+    const totalHeight = screenHeight - headerHeight;
+
+    // Actualiza el estilo del contenedor (por ejemplo, contenido)
+    const content = document.getElementById('MainPage');
+    if (content) {
+        content.style.height = `${totalHeight}px`; // Aplica la nueva altura
+    }
+});
+window.addEventListener('DOMContentLoaded', () => {
+    const headerElement = document.getElementById('header');
+    const headerHeight = headerElement ? headerElement.offsetHeight : 0; // Si 'header' es null, usa 0 como altura.
+    const screenHeight = window.innerHeight;
+    const totalHeight = screenHeight - headerHeight;
+
+    // Actualiza el estilo del contenedor (por ejemplo, contenido)
+    const content = document.getElementById('MainPage');
+    if (content) {
+        content.style.height = `${totalHeight}px`; // Aplica la nueva altura
+    }
+});
