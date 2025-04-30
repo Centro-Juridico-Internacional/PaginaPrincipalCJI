750c8e7cf5e292d355b37a7f8860d576a0ca2a9b
Desarrollo1 <desarrollo1@centrojuridicointernacional.com>
2025-04-30 13:09:13 -0500

    `Refactor Astro and JavaScript code for layout and carousel functionality`

--- src/components/YouTubeLite.jsx
+++ src/components/YouTubeLite.jsx
@@ -1,13 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function YouTubeLite({ videoid, id }) {
+	// Estado para manejar si el componente 'lite-youtube' ha sido cargado
+	const [loaded, setLoaded] = useState(false);
+
 	useEffect(() => {
 		if (typeof window !== 'undefined') {
+			// Importamos el componente 'lite-youtube' solo en el cliente
 			import('@justinribeiro/lite-youtube').then(() => {
+				setLoaded(true); // Marcamos que 'lite-youtube' ha sido cargado
 				console.log('lite-youtube cargado correctamente');
 			});
 		}
 	}, []);
+
+	if (!loaded) {
+		// Mientras el componente no esté cargado, mostramos un fallback o no renderizamos nada
+		return (
+			<div className="flex h-full w-full items-start justify-center">
+				{/* Muestra un enlace a YouTube si 'lite-youtube' no está cargado */}
+				<a className="lite-youtube-fallback" href={`https://www.youtube.com/watch?v=${videoid}`}>
+					Ver en YouTube
+				</a>
+			</div>
+		);
+	}
 
 	return (
 		<div className="flex h-full w-full items-start justify-center">
