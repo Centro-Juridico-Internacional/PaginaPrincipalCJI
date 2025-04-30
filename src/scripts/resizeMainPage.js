c4577b266702b97a9dc58a8a8b9d3a07a13e1fc9
Desarrollo1 <desarrollo1@centrojuridicointernacional.com>
2025-04-30 13:34:27 -0500

    `Added script to dynamically resize main page content based on header height and window size`

--- src/scripts/resizeMainPage.js
+++ src/scripts/resizeMainPage.js
@@ -1,3 +1,16 @@
+document.addEventListener('astro:page-load', () => {
+    const headerElement = document.getElementById('header');
+    const headerHeight = headerElement ? headerElement.offsetHeight : 0; // Si 'header' es null, usa 0 como altura.
+    const screenHeight = window.innerHeight;
+    const totalHeight = screenHeight - headerHeight;
+
+    // Actualiza el estilo del contenedor (por ejemplo, contenido)
+    const content = document.getElementById('MainPage');
+    if (content) {
+        content.style.height = `${totalHeight}px`; // Aplica la nueva altura
+    }
+});
+
 window.addEventListener('resize', () => {
     const headerElement = document.getElementById('header');
     const headerHeight = headerElement ? headerElement.offsetHeight : 0; // Si 'header' es null, usa 0 como altura.
